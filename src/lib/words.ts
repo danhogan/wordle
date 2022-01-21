46d095754f8fc618321d42c2b963134a75b3d961
Dan Hogan <danhogan78@gmail.com>
2022-01-21 11:29:44 -0500

    does this really stop it from buildling?

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -14,9 +14,9 @@ export const isWinningWord = (word: string) => {
 
 export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
-  const epochMs = 1641013200000
-  const now = Date.now()
-  const msInDay = 86400000
+  // const epochMs = 1641013200000
+  // const now = Date.now()
+  // const msInDay = 86400000
   const index = 0 //Math.floor((now - epochMs) / msInDay)
 
   return {
