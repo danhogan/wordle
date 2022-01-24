2f42c479491a981d7e73a02c4c7f0e1d4ed0d194
Dan Hogan <danhogan78@gmail.com>
2022-01-24 10:43:05 -0500

    some more words

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -1,3 +1,13 @@
 export const WORDS = [
   'farts',
+  'wrong',
+  'igloo',
+  'fishy',
+  'thumb',
+  'grief',
+  'pizza',
+  'trick',
+  'mince',
+  'trout',
+  'large'
 ]
--- src/lib/words.ts
+++ src/lib/words.ts
@@ -14,10 +14,10 @@ export const isWinningWord = (word: string) => {
 
 export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
-  // const epochMs = 1641013200000
-  // const now = Date.now()
-  // const msInDay = 86400000
-  const index = 0 //Math.floor((now - epochMs) / msInDay)
+  const epochMs = 1641013200000
+  const now = Date.now()
+  const msInDay = 86400000
+  const index = Math.floor((now - epochMs) / msInDay) - 22
 
   return {
     solution: WORDS[index].toUpperCase(),
